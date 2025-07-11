3dcd8aacbd2b958ecbfe798809d122f353a432de
DJAZERTY <djazerty.off@gmail.com>
2025-07-11 18:31:19 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -209,7 +209,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function addPlayCount(title) {
-  fetch('https://dj-azerty-blog.glitch.me/increment', {
+  fetch('https://vortex-music-backend.onrender.com/increment', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ title })
